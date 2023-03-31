483bf74de4b056c14acbf41eaf07b0f887920c76
Agung Ma'ruf <1911500518@student.budiluhur.ac.id>
2023-03-31 09:06:02 +0700

    update assignment 6

---
+++ Go Programming Microservices into The Specialization/challenge-tiga/config/database.go
@@ -0,0 +1,14 @@
+package config
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewDB(config *DBConfig) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		config.Host, config.Port, config.Username, config.Password, config.Name)
+
+	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
+}
